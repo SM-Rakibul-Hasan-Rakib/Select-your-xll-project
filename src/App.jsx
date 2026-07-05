3d42a41fcece9a2e018f03cf37e4de761942942d
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2026-07-05 22:30:16 +0600

    toggle btn section done

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ const fetchPlayers = async () => {
 };
 
 function App() {
+  const [toggle, setToggle] = useState(true);
+
   const playersPromise = fetchPlayers();
 
   return (
@@ -19,23 +21,37 @@ function App() {
       <div className="max-w-screen-xl mx-auto">
         <Navbar></Navbar>
         <div className="border border-red-500  max-w-[1200px] mx-auto flex justify-between items-center p-4 mt-4">
-          <h1> Available Player</h1>
+          <h1 className="font-bold text-2xl"> Available Player</h1>
 
-          <div>
-            <button>Available</button>
-            <button>
-              Selected <span>0</span>
+          <div className="font-bold">
+            <button
+              onClick={() => setToggle(true)}
+              className=" py-3 px-4 border-1 border-gray-400 rounded-l-2xl border-r-0 bg-[#E7FE29]"
+            >
+              Available
+            </button>
+            <button
+              onClick={() => setToggle(false)}
+              className=" py-3 px-4 border-1 border-gray-400 rounded-r-2xl border-l-0"
+            >
+              Selected <span>(0)</span>
             </button>
           </div>
         </div>
-        <Suspense
-          fallback={
-            <span className="loading loading-spinner loading-xl"></span>
-          }
-        >
-          <AvailablePlayers playersPromise={playersPromise}></AvailablePlayers>
-        </Suspense>
-        <SelectedPlayers></SelectedPlayers>
+
+        {toggle === true ? (
+          <Suspense
+            fallback={
+              <span className="loading loading-spinner loading-xl"></span>
+            }
+          >
+            <AvailablePlayers
+              playersPromise={playersPromise}
+            ></AvailablePlayers>
+          </Suspense>
+        ) : (
+          <SelectedPlayers></SelectedPlayers>
+        )}
       </div>
 
       {/* available players */}
